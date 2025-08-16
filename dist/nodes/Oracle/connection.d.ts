853af36c54d4bec9177a392b4e9dcd75b733b262
Jônatas Meireles <jonales@gmail.com>
2025-08-15 23:48:47 -0300

    Remoção
    Remoção da pasta dist

--- dist/nodes/Oracle/connection.d.ts
+++
@@ -1,9 +0,0 @@
-import { Connection } from 'oracledb';
-import { DatabaseConnection } from './interfaces/database.interface';
-import { OracleCredentials } from './types/oracle.credentials.type';
-export declare class OracleConnection implements DatabaseConnection {
-    private databaseConfig;
-    constructor(credentials: OracleCredentials);
-    getConnection(): Promise<Connection>;
-}
-//# sourceMappingURL=connection.d.ts.map
